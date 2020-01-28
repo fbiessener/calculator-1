5c93e06f3399f396860a0630dbf6d58ec3788074
Your Name <you@example.com>
2020-01-28 13:42:00 -0800

    Fixed add()

--- arithmetic.py
+++ arithmetic.py
@@ -4,32 +4,39 @@
 def add(num1, num2):
     """Return the sum of the two inputs."""
 
-    return 10
+    return num1 + num2
 
 
 def subtract(num1, num2):
     """Return the second number subtracted from the first."""
+    print("This is broken")
 
 
 def multiply(num1, num2):
     """Multiply the two inputs together."""
+    print("This is broken")
 
 
 def divide(num1, num2):
     """Divide the first input by the second and return the result."""
+    print("This is broken")
 
 
 def square(num1):
     """Return the square of the input."""
+    print("This is broken")
 
 
 def cube(num1):
     """Return the cube of the input."""
+    print("This is broken")
 
 
 def power(num1, num2):
     """Raise num1 to the power of num2 and return the value."""
+    print("This is broken")
 
 
 def mod(num1, num2):
     """Return the remainder of num1 / num2."""
+    print("This is broken")
